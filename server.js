04548665c0d5d89251e44e6ed8dab779f7f3a14f
StAmirey <owamirey@gmail.com>
2020-02-19 14:50:52 +0100

    Node JS server
    Started to add the Node JS server that will power the site and that will enable us to send emails to the user.
    Changed some general css

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 // https://www.w3schools.com/nodejs/nodejs_email.asp
+
+
+var app = require('express')();
+var http = require('http').createServer(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
